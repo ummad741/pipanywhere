c3b5e2fa36426bdd7087bf9fc3f1f070c549db7b
ummad741 <ummad741@gmail.com>
2023-12-08 16:58:45 +0500

    'static'

--- Core/settings.py
+++ Core/settings.py
@@ -137,7 +137,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/home/ummad/static'
+MEDIA_ROOT = '/home/ummad/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
